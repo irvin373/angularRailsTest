c4d4803afa0bfd3d7b057934f2b7483c6684258e
Rafael Ramírez <mrsblast@gmail.com>
2016-06-21 22:14:40 -0400

    md-autocomplete product companies.

--- app/assets/javascripts/angular/controller/angular_app.js
+++ app/assets/javascripts/angular/controller/angular_app.js
@@ -1,4 +1,4 @@
 // app/assets/javascripts/angular/controllers/users_controllers.js
-var myApp = angular.module('myapplication',['ngRoute','ngResource','ui.bootstrap']);
+var myApp = angular.module('myapplication',['ngRoute','ngResource','ngMaterial','ui.bootstrap']);
 //angular.module('myapplication', ['ngAnimate', 'ui.bootstrap']);
 
--- app/assets/javascripts/angular/controller/companys_controllers.js
+++ app/assets/javascripts/angular/controller/companys_controllers.js
@@ -1,13 +1,59 @@
 myApp
     .controller("CompanyListCtr",['$scope','$resource','$http','Companys','Company',companyListCtr])
-    .controller("CompanyAutoCompletCtr",['$scope','Companys',companyAutoCompleteCtr])
+    .controller("companyAutoCompleteCtr",['$scope','$q','$log','Companys',companyAutoCompleteCtr])
 
-function companyAutoCompleteCtr($scope,Companys){
+function companyAutoCompleteCtr($scope,$q,$log,Companys){
     var self = this;
-    self.companys = loadCompanys();
+    self.isDisabled = false;
+    self.companys = undefined;
     self.querySearch = querySearch;
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange = searchTextChange;
+    self.newCompany = newCompany;
+    loadCompanys();
+
+    function newCompany(){
+        console.log("Caracteristica por implementar");
+    }
+
+    function loadCompanys(){
+       var promise = Companys.query().$promise;
+       promise
+           .then(function(companys){
+               self.companys = companys.map(function(company){
+                   return ({
+                       value: company.line.toLowerCase(),
+                       line: company.line,
+                       id: company.id
+                   });
+               });
+           },function(err){
+               console.log(err);
+           });
+    }
+    
+    function querySearch(query){
+        var results = query ? self.companys.filter(createFilterFor(query)) : self.companys;
+        return results;
+    
+    }
+
+    function createFilterFor(query){
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(company){
+            return (company.value.indexOf(lowercaseQuery) === 0);
+        
+        };
+    }
+
+    function selectedItemChange(item){
+        $log.info('Item changed to ' + JSON.stringify(item));
+    
+    }
+
+    function searchTextChange(text){
+        $log.info('Text changed to ' + text);
+    }
 }
 
 function companyListCtr($scope,$resource,$http,Companys,Companys){
